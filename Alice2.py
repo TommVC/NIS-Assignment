a6f99b971c6c659ec3622455dfc88c41bcc37fd9
Tom <tomvancoller@gmail.com>
2024-05-12 18:19:49 +0200

    Diffie Hellman implemented. Next: Add end to end encryption and decryption using shared key.

--- Alice2.py
+++ Alice2.py
@@ -1,8 +1,14 @@
 import socket
 import threading
 import time
+import base64
+import hashlib
+from Cryptodome.Cipher import AES
+import DiffieHellman
 
-CONNECTED = False
+diffieHellman = DiffieHellman.DiffieHellman()
+#print("PrivInt:", diffieHellman.privNum)
+sharedKey = None
 def listen():
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     ip = "127.0.0.1"
@@ -13,12 +19,20 @@ def listen():
     listen_socket.listen(0)
     print(f"Listening on {ip}:{port}")
     client_socket, client_address = listen_socket.accept()
+
+    #obtaining shared secret key
+    secNum = eval(client_socket.recv(1024).decode("utf-8"))
+    #print("SecNum:" + str(secNum))
+    diffieHellman.generateSecretKey(secNum)
+    sharedKey = diffieHellman.getSecretKey()
+    #print("SK:" + str(sharedKey))
+
     msg = ""
     while not msg == "Q":
         msg = client_socket.recv(1024)
         msg = msg.decode("utf-8")
         if not(msg=="Q"):
-            print("\n[Alice]:" + msg)
+            print("\n[Bob]:" + msg)
     return
 
 def connect():
@@ -34,20 +48,24 @@ def connect():
         except:
             print("No one to connect to")
             time.sleep(5)
+    
+    #send secret integer for diffie hellman
+    secInt = diffieHellman.getSecretInteger()
+    #print("SecInt:" + str(secInt))
+    sendSocket.send(str(secInt).encode("utf-8"))
 
     msg = ""
     while not msg == "Q":
-        msg = send(sendSocket)
+        msg = sendMessage(sendSocket)
         time.sleep(1)
     return
             
 
-def send(skt):
-    msg = input("Send a message to Alice: ")
+def sendMessage(skt):
+    msg = input("Send a message to Bob: ")
     skt.send(msg.encode("utf-8"))
     return msg
 
-#main
 def main():
     listenThread = threading.Thread(target=listen)
     listenThread.start()
