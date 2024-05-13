fe57d293e8dfee0e795f120f4495387326a1acde
Willem <willemvryswyck@outlook.com>
2024-05-13 19:44:22 +0200

    Asymmetric key encryption working one way

--- Alice2.py
+++ Alice2.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import time
+from rsa_python import rsa
 
 CONNECTED = False
 def listen():
@@ -18,7 +19,7 @@ def listen():
         msg = client_socket.recv(1024)
         msg = msg.decode("utf-8")
         if not(msg=="Q"):
-            print("\n[Alice]:" + msg)
+            print("\n[Bob]:" + msg)
     return
 
 def connect():
@@ -30,26 +31,71 @@ def connect():
             # establish connection with server
             sendSocket.connect((serverIP, serverPort))
             print("Connected")
+            verify_outgoing(sendSocket)  # Send public key to receiver
+            print("finished being verified")
             break
         except:
             print("No one to connect to")
             time.sleep(5)
 
     msg = ""
-    while not msg == "Q":
-        msg = send(sendSocket)
-        time.sleep(1)
+#    while not msg == "Q":
+#        msg = send(sendSocket)
+#        time.sleep(1)
+    return
+
+
+def verify_outgoing(sskt):
+    try:
+        key_pair = rsa.generate_key_pair(1024)
+        publicKey = key_pair["public"]
+        privateKey = key_pair["private"]
+        keyModulus = key_pair["modulus"]
+
+        msg = "I am Alice"
+        sskt.send(msg.encode("utf-8"))
+
+        receive = sskt.recv(1024)
+        nonce = receive.decode("utf-8")  # receive nonce
+        print("Received nonce: " + nonce)
+
+        encrypted_nonce = rsa.encrypt(nonce, privateKey, keyModulus)  # Encrypt with private key
+        msg = encrypted_nonce
+        sskt.send(msg.encode("utf-8"))  # send encrypted nonce
+        print("Sent nonce: " + msg)
+        time.sleep(2)
+        msg = "END"
+        sskt.send(msg.encode("utf-8"))  # specify end of encrypted nonce
+
+        receive = sskt.recv(1024)
+        receive = receive.decode("utf-8")  # receive request
+        print("Received message: " + receive)
+
+        publicKey = str(publicKey)  # Only strings can be encoded
+        keyModulus = str(keyModulus)
+
+        msg = publicKey
+        sskt.send(msg.encode("utf-8"))  # send public key
+        print("Public key sent: " + msg)
+        time.sleep(2)  # So they are in two different messages
+        msg = keyModulus
+        sskt.send(msg.encode("utf-8"))  # send key modulus
+        print("Public modulus sent: " + msg)
+
+    except Exception as e:
+        print(e)
+
     return
             
 
 def send(skt):
-    msg = input("Send a message to Alice: ")
+    msg = input("Send a message to Bob: ")
     skt.send(msg.encode("utf-8"))
     return msg
 
 def main():
-    listenThread = threading.Thread(target=listen)
-    listenThread.start()
+#    listenThread = threading.Thread(target=listen)
+#    listenThread.start()
     connectThread = threading.Thread(target=connect)
     connectThread.start()
 
